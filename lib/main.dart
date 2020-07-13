aea81525344b51b98aa4dec242af021ca41a2fb4
yx.Zhang <yx.zhang@byteflyer.com>
2020-07-13 17:14:43 +0800

    TabBar：用标签形式展示内容

--- lib/main.dart
+++ lib/main.dart
@@ -18,25 +18,40 @@ class App extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigration',
-          onPressed: () => debugPrint('Navigration button is pressed.'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: 'Navigration',
+              onPressed: () => debugPrint('Navigration button is pressed.'),
+            ),
+            title: Text('DEMO'),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: 'Navigration',
+                onPressed: () => debugPrint('Search button is pressed.'),
+              ),
+            ],
+            elevation: 0.0,
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.local_florist)),
+                Tab(icon: Icon(Icons.change_history)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            )),
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+            Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+            Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+          ],
         ),
-        title: Text('DEMO'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Navigration',
-            onPressed: () => debugPrint('Search button is pressed.'),
-          ),
-        ],
-        elevation: 0.0,
       ),
-      body: null,
     );
   }
 }
