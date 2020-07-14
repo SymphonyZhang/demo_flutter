28a5b04d84e9398f35ec091b26318ee6fa0d09c2
yx.Zhang <yx.zhang@byteflyer.com>
2020-07-14 11:53:27 +0800

    BottomNavigrationBar：底部导航栏

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,28 @@ class Home extends StatelessWidget {
           ],
         ),
         drawer: DrawerDemo(),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          fixedColor: Colors.black,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.explore),
+              title: Text('Explore'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history),
+              title: Text('History'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              title: Text('List'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('My'),
+            ),
+          ],
+        ),
       ),
     );
   }
